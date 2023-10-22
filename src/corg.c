99dd8988df552ae8140794e43031339fc624ce56
Ryan Gragg <rygragg@gmail.com>
2023-10-22 15:41:54 -0700

    close #56

--- src/corg.c
+++ src/corg.c
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2023 Ryan Gragg.
+ *
+ * This file is part of Corg.
+ *
+ * Corg is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Corg is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with Corg. If not, see <https://www.gnu.org/licenses/>. 
+ */
+
 #include <curses.h>
 #include <stdlib.h>
 #include <stdio.h>
